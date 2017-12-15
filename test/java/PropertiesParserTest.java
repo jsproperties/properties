4d023901aa02da0a80e51444a3783d38001ed144
pallxk <github@pallxk.com>
2017-12-15 15:58:31 +0800

    Add a Java implementation and example properties for manual test

---
+++ test/java/PropertiesParserTest.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class PropertiesParserTest {
+  public static void main(String args[]) {
+    Properties properties = new Properties();
+
+    // Read input from stdin
+    try {
+      properties.load(System.in);
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    // Print name value pairs to stdout
+    System.out.print(properties);
+  }
+}
