5e9a8f1f66204a0ce6e8865283fda850a15336b3
pallxk <github@pallxk.com>
2017-12-22 12:07:15 +0800

    test: Test all possible parser options

--- test/js/parse.js
+++ test/js/parse.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+const _ = require('lodash');
 const tap = require('tap');
 
 const PropertiesParser = require('../..');
+const optionsToTest = require('./parser-options');
 
 const dataDir = path.resolve(__dirname, '../data');
 const rproperties = /\.properties$/;
@@ -14,32 +16,64 @@ const gen = process.argv[2] === '--gen';
 // Get .properties test files
 let filenames = fs.readdirSync(dataDir).filter(f => rproperties.test(f));
 if (!gen) {
-  tap.plan(filenames.length);
+  tap.plan(filenames.length * optionsToTest.length);
 }
 
-// Parse and compare
+// Parse, and snapshot or compare
 filenames.forEach(filename => {
   // Get input
   let input = fs.readFileSync(path.join(dataDir, filename), 'utf8');
 
-  // Generate output
-  let properties = PropertiesParser.parse(input);
-  let actual = JSON.stringify(properties, null, 2);
-
   // Write output or do the test
   if (gen) {
+    // Parse with all options on
+    let options = {
+      all: true,
+      original: true,
+      location: true,
+    };
+
+    // Generate output
+    let actualEntries = PropertiesParser.parse(input, options);
+    let actualString = JSON.stringify(actualEntries, null, 2);
+
     // Write output for later tests
     fs.writeFileSync(
       path.join(dataDir, filename.replace(rproperties, '.json')),
-      actual);
+      actualString);
   } else {
-    // Do the test
-    let expected = fs.readFileSync(
+    // Get snapshot output
+    let snapshotString = fs.readFileSync(
       path.join(dataDir, filename.replace(rproperties, '.json')), 'utf8');
-    if (actual === expected) {
-      tap.pass(`${filename} passed.`);
-    } else {
-      tap.fail(`${filename} failed.`);
+    let snapshotEntries = JSON.parse(snapshotString);
+
+    // Test each options used when parsing
+    for (options of optionsToTest) {
+      // Generate output
+      let actualEntries = PropertiesParser.parse(input, options);
+
+      // Make a clone as we are going to modify what's expected
+      let expectedEntries = _.cloneDeep(snapshotEntries);
+
+      // Calculate what's expected based on parser options
+      if (!options.all) {
+        expectedEntries = _.filter(expectedEntries, e => {
+          return e[0] != null && e[1] != null;
+        });
+      }
+      if (!options.original) {
+       expectedEntries.forEach(e => e.splice(2, 1));
+      }
+      if (!options.location) {
+        expectedEntries.forEach(e => e.splice(-1, 1));
+      }
+
+      // Do the test
+      if (_.isEqual(actualEntries, expectedEntries)) {
+        tap.pass(`${filename} ${JSON.stringify(options)} passed.`);
+      } else {
+        tap.fail(`${filename} ${JSON.stringify(options)} failed.`);
+      }
     }
   }
 });
---
+++ test/js/parser-options.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {},
+  {
+    all: false,
+    original: false,
+    location: false,
+  },
+  {
+    all: true,
+    original: false,
+    location: false,
+  },
+  {
+    all: false,
+    original: true,
+    location: false,
+  },
+  {
+    all: false,
+    original: false,
+    location: true,
+  },
+  {
+    all: false,
+    original: true,
+    location: true,
+  },
+  {
+    all: true,
+    original: false,
+    location: true,
+  },
+  {
+    all: true,
+    original: true,
+    location: false,
+  },
+  {
+    all: true,
+    original: true,
+    location: true,
+  },
+];
