a00cddd562893615ce3e0425354a75f7f385971f
pallxk <github@pallxk.com>
2020-03-18 16:18:36 +0800

    chore: Update webpack config to use mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
+  mode: 'development',
   output: {
     filename: 'properties.js',
     sourceMapFilename: 'properties.map',
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,17 +5,13 @@ const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
+  mode: 'production',
   output: {
     filename: 'properties.min.js',
     sourceMapFilename: 'properties.min.map',
   },
   devtool: 'source-map',
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
     new UglifyjsWebpackPlugin({
       sourceMap: true,
     }),
